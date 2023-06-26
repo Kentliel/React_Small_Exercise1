208f904c7c6ae7f04d681ec733d3f6ab200f03a5
Kentliel <kent.iluminati@gmail.com>
2023-06-26 09:29:40 +0200

    first commit

---
+++ src/component/Nav.js
@@ -0,0 +1,29 @@
+// const NavComplete = () => {
+//     return (
+//         <div>
+         
+//             <div className="exerciseTwo">
+//                 <h1>Exercise 2</h1>
+//                 <div className="backgroundBlue">
+//                     <p>Increment the number when you click on the buttons and display the result of the operation.</p>
+//                     <p>You can only use 3 states.</p>
+//                 </div>
+//             </div>
+//             <div className="exerciseTree">
+//                 <h1>Exercise 3</h1>
+//                 <div className="backgroundBlue">
+//                 <p><u>Itertion 1:</u>Give the value of "x" from <span>ComponentA</span> to <span>ComponentC</span></p>
+//                 <p><u>Iteration 2:</u>Change the "x" when clicking on the button "x++"</p>
+//                 </div>
+//             </div>
+//             <div className="exerciseFour">
+//                 <h1>Exercise 4</h1>
+//                 <div className="backgroundBlue">
+//                 <p>When the user type some information in the input, it should change the list of prime numbers.</p>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default NavComplete;
